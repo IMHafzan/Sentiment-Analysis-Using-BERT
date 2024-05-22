043a0bc342b16ca541c9bf0c2d4677b458122fbf
IMHafzan <87221832+IMHafzan@users.noreply.github.com>
2024-05-22 19:33:16 +0800

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,22 +1,26 @@
-from src.data_preprocessing import load_data, preprocess_data, tokenize_data, split_data
 from transformers import BertTokenizer
+from src.data_preprocessing import load_data, preprocess_data, tokenize_data
 import torch
 
 
 def main():
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    df = load_data('../data/imdb-movies-dataset.csv')
-    df, le = preprocess_data(df)
-    input_ids, attention_masks, labels = tokenize_data(df, tokenizer, max_length=128)
-    train_inputs, val_inputs, train_masks, val_masks, train_labels, val_labels = split_data(input_ids, attention_masks,
-                                                                                            labels)
+    # Load the dataset
+    file_path = '../data/imdb-movies-dataset.csv'
+    df = load_data(file_path)
 
-    torch.save(train_inputs, '../data/train_inputs.pt')
-    torch.save(val_inputs, '../data/val_inputs.pt')
-    torch.save(train_masks, '../data/train_masks.pt')
-    torch.save(val_masks, '../data/val_masks.pt')
-    torch.save(train_labels, '../data/train_labels.pt')
-    torch.save(val_labels, '../data/val_labels.pt')
+    # Preprocess the data
+    df, le = preprocess_data(df)
+
+    # Initialize the BERT tokenizer
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    # Tokenize the data
+    input_ids, attention_masks, labels = tokenize_data(df, tokenizer, max_length=128)
+
+    # Save the tokenized data
+    torch.save(input_ids, '../data/input_ids.pt')
+    torch.save(attention_masks, '../data/attention_masks.pt')
+    torch.save(labels, '../data/labels.pt')
 
 
 if __name__ == "__main__":

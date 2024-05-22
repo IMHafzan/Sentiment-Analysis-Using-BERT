986fb761ebb2981d30993c71f8bbc4f17a0f03fe
IMHafzan <87221832+IMHafzan@users.noreply.github.com>
2024-05-22 19:35:13 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,29 +1,34 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset, RandomSampler, SequentialSampler
+from torch.utils.data import DataLoader, TensorDataset, RandomSampler
 from transformers import BertForSequenceClassification, AdamW, get_linear_schedule_with_warmup
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 def load_data():
-    train_inputs = torch.load('../data/train_inputs.pt')
-    val_inputs = torch.load('../data/val_inputs.pt')
-    train_masks = torch.load('../data/train_masks.pt')
-    val_masks = torch.load('../data/val_masks.pt')
-    train_labels = torch.load('../data/train_labels.pt')
-    val_labels = torch.load('../data/val_labels.pt')
+    input_ids = torch.load('../data/input_ids.pt')
+    attention_masks = torch.load('../data/attention_masks.pt')
+    labels = torch.load('../data/labels.pt')
 
-    train_data = TensorDataset(train_inputs, train_masks, train_labels)
-    val_data = TensorDataset(val_inputs, val_masks, val_labels)
-
-    return train_data, val_data
+    return train_test_split(input_ids, attention_masks, labels, test_size=0.1, random_state=42)
 
 
-def train_model(train_data, val_data, batch_size=16, epochs=4):
+def train_model(train_data, val_data, epochs=4):
+    train_inputs, train_masks, train_labels = train_data
+    val_inputs, val_masks, val_labels = val_data
+
+    train_dataset = TensorDataset(train_inputs, train_masks, train_labels)
+    val_dataset = TensorDataset(val_inputs, val_masks, val_labels)
+
     train_dataloader = DataLoader(
-        train_data, sampler=RandomSampler(train_data), batch_size=batch_size
+        train_dataset,
+        sampler=RandomSampler(train_dataset),
+        batch_size=16
     )
+
     val_dataloader = DataLoader(
-        val_data, sampler=SequentialSampler(val_data), batch_size=batch_size
+        val_dataset,
+        batch_size=16
     )
 
     model = BertForSequenceClassification.from_pretrained(
@@ -34,35 +39,40 @@ def train_model(train_data, val_data, batch_size=16, epochs=4):
     )
 
     optimizer = AdamW(model.parameters(), lr=2e-5, eps=1e-8)
+
     total_steps = len(train_dataloader) * epochs
 
-    scheduler = get_linear_schedule_with_warmup(
-        optimizer, num_warmup_steps=0, num_training_steps=total_steps
-    )
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
 
-    loss_fn = nn.CrossEntropyLoss()
-
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model.to(device)
+    model.train()
 
     for epoch in range(epochs):
-        model.train()
         total_loss = 0
 
-        for batch in train_dataloader:
-            b_input_ids, b_input_mask, b_labels = tuple(t.to(device) for t in batch)
+        for step, batch in enumerate(train_dataloader):
+            b_input_ids, b_input_mask, b_labels = batch
 
             model.zero_grad()
+
             outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+
             loss = outputs.loss
             total_loss += loss.item()
+
             loss.backward()
 
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             optimizer.step()
             scheduler.step()
 
-        avg_train_loss = total_loss / len(train_dataloader)
-        print(f"Epoch {epoch + 1}, Loss: {avg_train_loss}")
+        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(train_dataloader)}")
 
     torch.save(model.state_dict(), '../models/bert_model.pth')
+
+
+if __name__ == "__main__":
+    train_inputs, val_inputs, train_masks, val_masks, train_labels, val_labels = load_data()
+
+    train_data = (train_inputs, train_masks, train_labels)
+    val_data = (val_inputs, val_masks, val_labels)
+
+    train_model(train_data, val_data, epochs=4)

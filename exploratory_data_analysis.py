abafdfbe48dd0084955abc093fd1f605576d3143
IMHafzan <87221832+IMHafzan@users.noreply.github.com>
2024-07-01 22:53:36 +0800

    Update exploratory_data_analysis.py

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Load data
+
 df = pd.read_csv('../data/imdb-movies-dataset.csv')
 
-# Display the first few rows of the dataset
 print(df.head())
 
-# Plot distribution of sentiments
 sns.countplot(x='sentiment', data=df)
 plt.title('Sentiment Distribution')
 plt.show()

2eb4bb54ba126c8914d1c066f2544e3fbc061755
IMHafzan <87221832+IMHafzan@users.noreply.github.com>
2024-07-01 22:54:56 +0800

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -4,20 +4,17 @@ import torch
 
 
 def main():
-    # Load the dataset
+
     file_path = '../data/imdb-movies-dataset.csv'
     df = load_data(file_path)
 
-    # Preprocess the data
+
     df, le = preprocess_data(df)
 
-    # Initialize the BERT tokenizer
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    # Tokenize the data
     input_ids, attention_masks, labels = tokenize_data(df, tokenizer, max_length=128)
 
-    # Save the tokenized data
     torch.save(input_ids, '../data/input_ids.pt')
     torch.save(attention_masks, '../data/attention_masks.pt')
     torch.save(labels, '../data/labels.pt')
